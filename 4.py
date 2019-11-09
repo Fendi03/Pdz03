4477573b235a1f72849a3e9beb604123bc13a93c
Fendi03 <57419280+Fendi03@users.noreply.github.com>
2019-11-09 21:57:08 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,15 @@
+print ('A'),
+
+print ('A'),
+
+print ('E'),
+
+print ('R'),
+
+print ('K'),
+
+print ('D'),
+
+print ('M'),
+
+print ('Y'),
---
+++ 5.py
@@ -0,0 +1,23 @@
+matrix = [
+
+    [5, 0, 6],
+
+    [2, 6, 3],
+    [3, 4, 1]
+
+]
+
+
+
+
+for x in range(0, len(matrix)):
+
+    for y in range(0, len(matrix[0])):
+
+        print (matrix[x][y], end=' '),
+
+    print
+
+
+
+print (5+6+1+3+6+6, end=' '),
